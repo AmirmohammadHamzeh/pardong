21251efaef544ad6b27c1cf98f50617961d00702
tandorost <amrimohammadhamzehi@gmail.com>
2025-06-24 16:19:42 +0330

    chore: response model for response of auth api

--- routes/authentication.py
+++ routes/authentication.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from pymongo.errors import DuplicateKeyError
 from Models import MemberModel
 from jwt_token import verify_token, create_jwt_token
 from database import Database
+from response_api import make_response
 
 
 async def get_members_collection():
@@ -18,9 +19,10 @@ async def register(user: MemberModel):
     members_collection = await get_members_collection()
     try:
         await members_collection.insert_one(user.dict())
-        return {"message": "User registered successfully"}
+        return make_response(message="User registered",
+                             status_code=status.HTTP_201_CREATED)
     except DuplicateKeyError:
-        raise HTTPException(status_code=400, detail="User ID already exists")
+        return make_response(message="user already exists",status_code=status.HTTP_409_CONFLICT)
 
 
 @router.get("/login/")
@@ -29,10 +31,10 @@ async def login(user_id: int):
     existing_user = await members_collection.find_one({"user_id": user_id})
 
     if not existing_user:
-        raise HTTPException(status_code=401, detail="User not found")
-
+        return make_response(message="User not found", status_code=404)
     token = create_jwt_token(user_id)
-    return {"token": token}
+
+    return make_response(message="successful", data={"token": token}, status_code=status.HTTP_202_ACCEPTED)
 
 
 @router.get("/get_user_data/", summary="Get User Data")
@@ -40,6 +42,7 @@ async def protected_route(user_data: dict = Depends(verify_token)):
     members_collection = await get_members_collection()
     existing_user = await members_collection.find_one({"user_id": int(user_data["sub"])})
     if not existing_user:
-        raise HTTPException(status_code=401, detail="User not found")
+        return make_response(message="User not found", status_code=status.HTTP_404_NOT_FOUND)
     del existing_user["_id"]
-    return existing_user
+    return make_response(message="successful", data=existing_user, status_code=status.HTTP_200_OK)
+
