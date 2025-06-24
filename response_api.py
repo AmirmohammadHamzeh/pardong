ad34991100fb1a913cf01851c0ed7b2fd5b45f8b
tandorost <amrimohammadhamzehi@gmail.com>
2025-06-24 16:20:38 +0330

    feat : create response model for response of all api

--- response_api.py
+++ response_api.py
@@ -1,14 +1,20 @@
 from fastapi.responses import JSONResponse
 from starlette import status as http_status
-from Models import ReturnModel
+
 
 def make_response(
-    status: bool = True,
-    message: str = None,
-    data: any = None,
-    status_code: int = http_status.HTTP_200_OK
+        message: str = None,
+        data: any = None,
+        status_code: int = http_status.HTTP_200_OK
 ):
+    response_data = {
+        "message": message,
+        "data": data
+    }
+
+    response_data = {k: v for k, v in response_data.items() if v is not None}
+
     return JSONResponse(
         status_code=status_code,
-        content=ReturnModel(status=status, message=message, data=data).dict()
+        content=response_data
     )
