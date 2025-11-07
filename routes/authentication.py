1b111b4ebb7c49af49f2ed0e02355d2e7892b09e
tandorost <amrimohammadhamzehi@gmail.com>
2025-11-07 21:01:39 +0330

    chore : refactor auth

--- routes/authentication.py
+++ routes/authentication.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi import APIRouter, HTTPException, Depends, status, Query
 from pymongo.errors import DuplicateKeyError
 from Models import MemberModel
 from jwt_token import verify_token, create_jwt_token
@@ -15,34 +15,30 @@ router = APIRouter()
 
 
 @router.post("/register/")
-async def register(user: MemberModel):
+async def register_user(user: MemberModel):
     members_collection = await get_members_collection()
     try:
         await members_collection.insert_one(user.dict())
         return make_response(message="User registered",
                              status_code=status.HTTP_201_CREATED)
     except DuplicateKeyError:
-        return make_response(message="user already exists",status_code=status.HTTP_409_CONFLICT)
-
-
-@router.get("/login/")
-async def login(user_id: int):
-    members_collection = await get_members_collection()
-    existing_user = await members_collection.find_one({"user_id": user_id})
-
-    if not existing_user:
-        return make_response(message="User not found", status_code=404)
-    token = create_jwt_token(user_id)
-
-    return make_response(message="successful", data={"token": token}, status_code=status.HTTP_202_ACCEPTED)
+        return make_response(message="user already exists", status_code=status.HTTP_409_CONFLICT)
 
 
 @router.get("/get_user_data/", summary="Get User Data")
-async def protected_route(user_data: dict = Depends(verify_token)):
+async def protected_route(user_id: int = Query(..., description="User_ID")):
     members_collection = await get_members_collection()
-    existing_user = await members_collection.find_one({"user_id": int(user_data["sub"])})
+    existing_user = await members_collection.find_one({"user_id": user_id})
     if not existing_user:
         return make_response(message="User not found", status_code=status.HTTP_404_NOT_FOUND)
     del existing_user["_id"]
     return make_response(message="successful", data=existing_user, status_code=status.HTTP_200_OK)
 
+
+@router.get("/check-registration/")
+async def check_registration(user_id: int = Query(..., description="User_ID")):
+    groups_collection = await get_members_collection()
+    user = await groups_collection.find_one({"user_id": user_id})
+    if not user:
+        return make_response(message="User is not register", status_code=status.HTTP_200_OK)
+    return make_response(message="User successfully Found", status_code=status.HTTP_202_ACCEPTED)
